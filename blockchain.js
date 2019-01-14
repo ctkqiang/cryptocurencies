a0ff42e7f59f65a776f645ec5b0dc21d3cbf1c03
JohnMelodyMel <44691957+johnmelodyme@users.noreply.github.com>
2019-01-14 08:28:48 +0800

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -64,3 +64,38 @@ console.log(JSON.stringify(johnCoin, null, 4)); // node blockchain.js
 johnCoin.chain[1].data = {amount: 100};
 johnCoin.chain[1].hash = johnCoin.chain[1].calculateHash();
 console.log("Is this blockchain valid?" + johnCoin.isChainValid());
+    //node blockchain.js
+
+    /* OUTPUT:
+
+        {
+    "chain": [
+        {
+            "index": "undefined01/01/2019{\"amount\":4}",
+            "timestamp": "01/01/2019",
+            "data": {
+                "amount": 4
+            },
+            "hash": "bf5f1f1e45d55835ac85c38eaa95ce2d0c061e3c2d5264e45c40c4f5e953530b"
+        },
+        {
+            "index": "[object Object]02/01/2019{\"amount\":10}",
+            "timestamp": "02/01/2019",
+            "data": {
+                "amount": 10
+            },
+            "previousHash": {
+                "index": "undefined01/01/2019{\"amount\":4}",
+                "timestamp": "01/01/2019",
+                "data": {
+                    "amount": 4
+                },
+                "hash": "bf5f1f1e45d55835ac85c38eaa95ce2d0c061e3c2d5264e45c40c4f5e953530b"
+            },
+            "hash": "f426434ace6097591448905e0bc9ea7840c95c9d662b3161617539756ab448f9"
+        }
+    ]
+}
+Is this blockchain valid?false
+
+    */

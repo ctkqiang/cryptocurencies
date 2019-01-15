3a38dc55eb79958a99cea5e7cd8ae86132c9cf4a
JohnMelodyMel <44691957+johnmelodyme@users.noreply.github.com>
2019-01-15 18:28:28 +0800

    Add files via upload

---
+++ bicoinprice.js
@@ -0,0 +1,20 @@
+/*
+Author: John Melody Melissa;
+proj: BITCOIN PRICE API
+*/
+const request = require('request');
+
+function BTC(){
+    return new Promise((resolve) => {
+
+        // send a request to blockchain
+        request('https://blockchain.info/de/ticker', (error, response, body) => {
+
+            // parse the json answer and get the current bitcoin value
+            const data = JSON.parse(body);
+            value = (parseInt(data.USD.buy, 10) + parseInt(data.USD.sell, 10)) / 2;
+            resolve(value);
+        });
+    });
+}
+BTC().then(val => console.log(val));
